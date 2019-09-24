a02020fbc33cb350be11151d52fa7f52d684f8ce
Viktoriia <viktoria.nosik@gmail.com>
2019-09-24 21:35:21 +0300

    fixed travis problems

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-'use strict'
+'use strict';
 
-var NUMBER = ['1', '2', '3', '4', '5', '6', '7', '8'];
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var map = document.querySelector('.map');
-var map__pins = document.querySelector('.map__pins');
+var mapPins = document.querySelector('.map__pins');
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
 var fragment = document.createDocumentFragment();
 
@@ -20,9 +19,10 @@ var chooseRandomInRange = function (min, max) {
 
 var shuffle = function (array) {
   var currentIndex = array.length;
-  var temporaryValue, randomIndex;
+  var temporaryValue;
+  var randomIndex;
 
-  while (0 !== currentIndex) {
+  while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
     temporaryValue = array[currentIndex];
@@ -62,7 +62,7 @@ var createRandomAdvert = function (i) {
       x: chooseRandomInRange(50, 930),
       y: chooseRandomInRange(130, 630),
     }
-  }
+  };
 };
 
 var generateAdverts = function (n) {
@@ -88,13 +88,11 @@ var renderFragment = function () {
   map.classList.remove('map--fadded');
   adverts = generateAdverts(8);
 
-  console.log(adverts);
-
   for (var i = 0; i < adverts.length; i++) {
     fragment.appendChild(renderPin(adverts[i]));
   }
 
-  map__pins.appendChild(fragment);
+  mapPins.appendChild(fragment);
 };
 
 renderFragment();
